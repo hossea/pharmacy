0bb012dfdaebc6047161b48e493385085c098a66
Your Name <your-email@example.com>
2024-12-21 16:53:15 +0300

    initial commit

---
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+
+class VerifyCsrfToken extends Middleware
+{
+    /**
+     * Indicates whether the XSRF-TOKEN cookie should be set on the response.
+     *
+     * @var bool
+     */
+    protected $addHttpCookie = true;
+
+    /**
+     * The URIs that should be excluded from CSRF verification.
+     *
+     * @var array<int, string>
+     */
+    protected $except = [
+        // Example:
+        // 'webhook/*',
+        // 'api/payment/*',
+    ];
+
+    /**
+     * Determine if the request has a valid CSRF token.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function tokensMatch($request)
+    {
+        $token = $this->getTokenFromRequest($request);
+
+        return is_string($this->encrypter->decrypt($token, false)) && parent::tokensMatch($request);
+    }
+}
